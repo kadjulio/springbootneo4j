c441bb03c79f5d442315c6200cf7c966d499ea32
Ogui Juliot <oguijulio@gmail.com>
2020-07-27 01:37:48 +0200

    Update

--- src/main/java/com/oguij/springbootneo4j/model/Position.java
+++ src/main/java/com/oguij/springbootneo4j/model/Position.java
@@ -1,25 +1,60 @@
 package com.oguij.springbootneo4j.model;
 
+
+import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.Relationship;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Position {
-    private float longitude;
-    private float latitude;
+    @Id private String longitude;
+    @Id private String latitude;
     private String Gitter_ID_100m;
 
-    private Position() {
-        // Empty constructor required as of Neo4j API 2.0.5
-    };
 
-    public Position(String Gitter_ID_100m) {
+    public Position(String Gitter_ID_100m, String latitude, String longitude) {
         this.Gitter_ID_100m = Gitter_ID_100m;
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 
-    public float getLongitude() {
+    /**
+     * Neo4j doesn't REALLY have bi-directional relationships. It just means when querying
+     * to ignore the direction of the relationship.
+     * https://dzone.com/articles/modelling-data-neo4j
+     */
+
+    @Relationship(type = "LOCATED_AT", direction = Relationship.UNDIRECTED)
+    public Set<Position> located_at;
+
+    public Position() {
+        // Empty constructor required as of Neo4j API 2.0.5
+    }
+
+
+    public void located_at(Position position) {
+        if (located_at == null) {
+            located_at =new HashSet<>();
+        }
+        located_at.add(position);
+    }
+
+    public String toString() {
+        return this.Gitter_ID_100m + "is the location of =>" +
+                Optional.ofNullable(this.located_at).orElse(Collections.emptySet()).stream()
+                        .map(Position::getGitter_ID_100m).collect(Collectors.joining());
+
+    }
+
+    public String getLongitude() {
         return longitude;
     }
 
-    public float getLatitude() {
+    public String getLatitude() {
         return latitude;
     }
 
@@ -27,11 +62,11 @@ public class Position {
         return Gitter_ID_100m;
     }
 
-    public void setLongitude(float longitude) {
+    public void setLongitude(String longitude) {
         this.longitude = longitude;
     }
 
-    public void setLatitude(float latitude) {
+    public void setLatitude(String latitude) {
         this.latitude = latitude;
     }
 
